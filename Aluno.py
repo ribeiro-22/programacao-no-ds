ce1a16d39c00345899e6dd5dcd39815988ace02b
ribeiro-22 <jean.silveira.ribeiro@escola.pr.gov.br>
2025-05-27 08:24:30 -0300

    Create Aluno.py

---
+++ Aluno.py
@@ -0,0 +1,25 @@
+import functools
+
+class Aluno:
+
+    def __init__(self, nome, nota):
+
+        self.nome = nome
+
+        self.nota = nota
+
+    def __repr__(self):
+
+        return f'{self.nome}: {self.nota}'
+
+
+
+def comparar_alunos(a, b):
+
+    return (a.nota > b.nota) - (a.nota < b.nota)
+
+alunos = [Aluno('Ana', 88), Aluno('Rafael', 92), Aluno('Paulo', 78)]
+
+alunos_ordenados = sorted(alunos, key=functools.cmp_to_key(comparar_alunos))
+
+print(alunos_ordenados)
