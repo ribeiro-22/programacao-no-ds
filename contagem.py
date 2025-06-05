4154908f32089aec9098c48c1ae3006b57bb287c
ribeiro-22 <jean.silveira.ribeiro@escola.pr.gov.br>
2025-06-05 12:01:10 -0300

    Create contagem.py

---
+++ contagem.py
@@ -0,0 +1,9 @@
+# contagem.py
+def contar_caracteres(frase):
+    return len(frase)
+
+# Entrada do usuário
+frase_usuario = input("Digite uma frase: ")
+
+# Saída
+print(f"O número de caracteres na frase é: {contar_caracteres(frase_usuario)}")
