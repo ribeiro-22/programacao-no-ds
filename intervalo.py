e709283d3375ce833021aa35978282bfeeb9681e
ribeiro-22 <jean.silveira.ribeiro@escola.pr.gov.br>
2025-06-05 11:51:45 -0300

    Create intervalo.py

---
+++ intervalo.py
@@ -0,0 +1,6 @@
+# Solicita ao usuário um número inteiro
+numero = int(input("Digite um número inteiro: "))
+
+# Gera e exibe a sequência de números de 0 até o número fornecido
+for i in range(numero + 1):
+    print(i)
