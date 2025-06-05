ccae3b46b362c1b7bebeca68b86eb966fef10685
ribeiro-22 <jean.silveira.ribeiro@escola.pr.gov.br>
2025-06-05 11:57:34 -0300

    Update tuplas.py

--- tuplas.py
+++ tuplas.py
@@ -1,32 +1,9 @@
-# Definição implícita de uma tupla
-tupla_implicita = (1, 2, 3, 4)
+# Solicita ao usuário as coordenadas geográficas (latitude e longitude)
+coordenadas = input("Digite a latitude e longitude separadas por vírgula: ").split(",")
 
-# Definição explícita de uma tupla
-lista = [5, 6, 7, 8]
-tupla_explicita = tuple(lista)
+# Converte os valores para números float e desempacota a tupla
+latitude, longitude = map(float, coordenadas)
 
-print("Tupla implícita:", tupla_implicita, "| Tipo:", type(tupla_implicita))
-print("Tupla explícita:", tupla_explicita, "| Tipo:", type(tupla_explicita)) 
-
-# Cria uma tupla
-empresas = ("Google", "Facebook", "Amazon") 
-# Tentativa de alteração 
-empresas[1] = "Samsung" 
-
-frutas = ["maçã", "banana", "laranja"]
-frutas.append("uva")
-
-print(frutas)
-
-frutas = ["maçã", "banana", "laranja", "banana"]
-frutas.remove("banana")
-
-print(frutas)
-
-# Definição de uma tupla
-frutas = ("maçã", "banana", "laranja", "uva")
-
-# Encontra o índice do elemento "laranja" na tupla
-indice_laranja = frutas.index("laranja")
-
-print("O índice de 'laranja' é:", indice_laranja)
+# Exibe as coordenadas
+print(f"Latitude: {latitude}")
+print(f"Longitude: {longitude}")
