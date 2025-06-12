cfcbca863866b710e1fd7e94bfb58c1db8bb5ba5
ribeiro-22 <jean.silveira.ribeiro@escola.pr.gov.br>
2025-06-12 11:48:58 -0300

    Create livros.py

---
+++ livros.py
@@ -0,0 +1,16 @@
+# Ordenando uma lista de títulos de livros
+titulos_livros = ["O Apanhador no Campo de Centeio", "1984", "Orgulho e Preconceito", "Dom Quixote"]
+titulos_livros.sort()
+print("Títulos ordenados:", titulos_livros)
+
+
+# Ordenando uma lista de sobrenomes de autores
+autores = ["Orwell", "Salinger", "Austen", "Cervantes"]
+autores.sort()
+print("Autores ordenados:", autores)
+
+
+# Ordenando uma lista de datas de publicação
+datas_publicacao = [1951, 1949, 1813, 1605]
+datas_publicacao_ordenadas = sorted(datas_publicacao)
+print("Datas de publicação ordenadas:", datas_publicacao_ordenadas)
