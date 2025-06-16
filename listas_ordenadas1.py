f65e3df3cae72e776605f4edc0ef02552b0b4577
ribeiro-22 <jean.silveira.ribeiro@escola.pr.gov.br>
2025-06-16 10:33:50 -0300

    Create  listas_ordenadas1.py

---
+++ listas_ordenadas1.py
@@ -0,0 +1,6 @@
+# 1. Lista de nomes de estudantes
+estudantes = ["Carlos", "Ana", "Pedro", "Beatriz", "Mariana"]
+
+# Use sort() para ordenar diretamente a lista original em ordem decrescente
+estudantes.sort(reverse=True) 
+print("Estudantes ordenados (descrescente):", estudantes)
