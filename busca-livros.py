144bfd37dccc7aab92c9f28dfbc3b7220dcbac59
ribeiro-22 <jean.silveira.ribeiro@escola.pr.gov.br>
2025-06-26 10:09:15 -0300

    Create busca-livros.py

---
+++ busca-livros.py
@@ -0,0 +1,6 @@
+# Solicita entrada de dado
+nome_usuario = input("Digite seu nome em letras minúsculas: ") 
+# Converte string em maiúsculas
+nome_formatado = nome_usuario.upper()
+# Mostra o resultado da conversão com upper()
+print(f"Seu nome formatado: {nome_formatado}")  
