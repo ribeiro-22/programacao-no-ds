088334653a8b92b3a38508e1cd8e9b8af63efd9c
ribeiro-22 <jean.silveira.ribeiro@escola.pr.gov.br>
2025-06-27 11:53:07 -0300

    Create conta-caracteres.py

---
+++ conta-caracteres.py
@@ -0,0 +1,7 @@
+Teste um código em Python para contar o número total de caracteres (incluindo espaços e pontuação) em uma string, usando o editor GDB Online.
+
+# Declara a variável para armazenar string
+texto = "Olá, mundo! Python é divertido." 
+# Aplica função len() na contagem de caracteres
+tamanho = len(texto) 
+print(f"O texto tem {tamanho} caracteres.")
