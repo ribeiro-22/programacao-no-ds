2664b828b9e113651b742a6ba4ebf864b6b69e9d
ribeiro-22 <jean.silveira.ribeiro@escola.pr.gov.br>
2025-11-06 11:59:42 -0300

    Create  calculaPreco.js

---
+++ calculaPreco.js
@@ -0,0 +1,22 @@
+function calcularPrecoFinal(produto) {
+  const desconto = produto.desconto ?? 0;
+
+  const precoFinal = produto.preco - (produto.preco * desconto / 100);
+  console.log(`Preço inicial de ${produto.nome}: R$ ${produto.preco.toFixed(2)}`);
+  console.log(`Preço final de ${produto.nome}: R$ ${precoFinal.toFixed(2)}`);
+
+}
+
+// Exemplo de uso
+
+const produto1 = {
+
+  nome: "Fone de ouvido",
+
+  preco: 200,
+
+  desconto: 25
+
+};
+
+calcularPrecoFinal(produto1);
